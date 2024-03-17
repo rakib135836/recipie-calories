1778f3f25f2392a39af28b513e285cbea381c4a0
rakib <rakib135836@gmail.com>
2024-03-17 09:26:44 +0600

    added main file

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      
+      <h1 className='text-2xl capitalize'>Recipe Calories </h1>
+     
+    </>
+  )
+}
+
+export default App
