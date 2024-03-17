5c41dde91beb5f72b67387c5ccfa174b95d36aed
rakib <rakib135836@gmail.com>
2024-03-17 10:55:41 +0600

    added main file

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 
 import './App.css'
 
+
+
 function App() {
  
 
   return (
     <>
       
-      <h1 className='text-2xl capitalize'>Recipe Calories </h1>
+    
      
     </>
   )
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,28 @@
+
+
+const header = () => {
+    return (
+        <div>
+             <div>
+              <div>
+            <h1 className="text-2xl font-bold">Recipe Calories</h1>
+
+            <div>
+                <ul>
+                    <li>Home</li>
+                    <li>Recipies</li>
+                    <li>About</li>
+                    <li>Search</li>
+                </ul>
+            </div>
+
+            <div>
+                
+            </div>
+        </div>
+        </div>
+        </div>
+    );
+};
+
+export default header;
